7d66bcb67843949fcd471a6a2687644384df8679
alexehouston <alexehouston@gmail.com>
2022-10-27 16:54:55 -0500

    enable google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pokébank' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google', { scope: ['profile', 'email'], prompt: 'select_account' }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google', { successRedirect: '/account', failureRedirect: '/index' }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    res.redirect('/index');
+  });
 });
 
 module.exports = router;
